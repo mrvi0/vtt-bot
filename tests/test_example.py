9c7dc4f20793310ec5388fc88c94de0978184572
b4dcat <support@b4dcat.ru>
2024-08-06 18:25:52 +0300

    docs: add inline comments

--- tests/test_example.py
+++ tests/test_example.py
@@ -4979,3 +4979,7 @@ from typing import Optional
 def improve_performance():
     '''Performance optimization'''
     pass
+
+def new_feature():
+    '''New feature implementation'''
+    return True
