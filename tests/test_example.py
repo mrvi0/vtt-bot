0cec6e10fe4049dac9f52bfcf7231560d6ae4346
b4dcat <support@b4dcat.ru>
2024-08-15 09:43:23 +0300

    refactor: simplify logic

--- tests/test_example.py
+++ tests/test_example.py
@@ -5141,3 +5141,7 @@ def improve_performance():
 def new_feature():
     '''New feature implementation'''
     return True
+
+def new_feature():
+    '''New feature implementation'''
+    return True
