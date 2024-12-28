13e568aaf350350f0755f419fcb065a904c6dc3f
b4dcat <support@b4dcat.ru>
2024-12-28 13:44:35 +0300

    chore: update dependencies

--- tests/test_example.py
+++ tests/test_example.py
@@ -6431,3 +6431,7 @@ def improve_performance():
 def new_feature():
     '''New feature implementation'''
     return True
+
+def improve_performance():
+    '''Performance optimization'''
+    pass
