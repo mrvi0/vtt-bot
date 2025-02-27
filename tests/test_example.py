3c6520ccd067e86fa9a249a99d8a5158949b469f
b4dcat <support@b4dcat.ru>
2025-02-27 15:32:32 +0300

    perf: optimize performance

--- tests/test_example.py
+++ tests/test_example.py
@@ -7811,3 +7811,7 @@ from typing import Optional
 def improve_performance():
     '''Performance optimization'''
     pass
+
+def improve_performance():
+    '''Performance optimization'''
+    pass
