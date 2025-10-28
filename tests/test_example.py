2a9b741481957265425022019532002274cf0081
b4dcat <support@b4dcat.ru>
2025-10-28 16:03:28 +0300

    docs: add inline comments

--- tests/test_example.py
+++ tests/test_example.py
@@ -14457,3 +14457,7 @@ def improve_performance():
 # FIXME: This needs optimization
 
 # NOTE: Important implementation detail
+
+def improve_performance():
+    '''Performance optimization'''
+    pass
