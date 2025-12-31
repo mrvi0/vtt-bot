85c9a2d301adce60967d09e41f94cb8717d2fc6f
b4dcat <support@b4dcat.ru>
2025-12-31 10:12:05 +0300

    docs: update documentation

--- tests/test_example.py
+++ tests/test_example.py
@@ -16017,3 +16017,7 @@ def improve_performance():
 def improve_performance():
     '''Performance optimization'''
     pass
+
+def improve_performance():
+    '''Performance optimization'''
+    pass
